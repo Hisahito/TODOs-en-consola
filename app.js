43efcccf5d9e706e91f4b6b5cd88b6dd1595d749
hisahito <hisahito706@gmail.com>
2022-09-07 23:06:28 -0500

    only commit, this was a practice in local totally

---
+++ app.js
@@ -0,0 +1,74 @@
+require('colors');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoCheckList} = require('./helpers/inquirer'); 
+const Tareas = require('./models/tareas');
+
+
+const main = async() => {
+
+   let opt = '';
+   const tareas = new Tareas();
+
+   const  tareasDB = leerDB();
+
+   if( tareasDB ){
+    // Establecer las tareas
+    tareas.cargarTareasFromArray( tareasDB );
+   }
+
+   do {
+        // Imprimir el menú
+         opt = await inquirerMenu();
+        
+         switch (opt) {
+                case '1':
+                    // Crear opción
+                    const desc = await leerInput('Descripción: ');
+                    tareas.crearTarea( desc );
+                break;
+
+                case '2':
+                    tareas.listadoCompleto();
+                break;
+
+                case '3':
+                    tareas.listarPendientesCompletadas(true);
+                break;
+
+                case '4':
+                    tareas.listarPendientesCompletadas( false );
+                break;
+
+                case '5': // Completado | pendiente
+                    const ids = await mostrarListadoCheckList( tareas.listadoArr );
+                    tareas.toggleCompletadas( ids );
+                break;
+
+                case '6':
+                    const id = await listadoTareasBorrar( tareas.listadoArr );
+                    if( id !== '0' ){
+                        const ok = await confirmar('¿Está seguro?');
+                        if( ok ){
+                            tareas.borrarTarea( id );
+                            console.log('Tarea borrada');
+                    }
+                }
+                break;
+         }
+
+         guardarDB( tareas.listadoArr );
+
+         
+
+         await pausa();
+
+   } while (opt !== '0');
+
+    
+
+    //pausa();
+
+}
+
+main();
